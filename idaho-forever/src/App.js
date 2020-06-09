5167de773feb347cca2216504cd6c5fd96ce03f5
Shawn McManus <xsvfan76@gmail.com>
2020-06-09 17:36:23 -0600

    added cards for home page

--- idaho-forever/src/App.js
+++ idaho-forever/src/App.js
@@ -5,9 +5,11 @@ import {
   Link,
   Redirect,
 } from 'react-router-dom';
+import Home from './components/Home.js';
 
 import './App.css';
 import NavBar from './components/NavBar.js';
+import Brews from './components/Brews.js';
 import image1 from './images/image-1.JPG';
 import logo from './images/Logo.JPG';
 
@@ -19,7 +21,8 @@ function App() {
         {/* <img src={logo} alt='logo' className='Logo' /> */}
         <Router>
           <NavBar />
-          {/* <Route exact path={'/'} component={Home} /> */}
+          <Route exact path={'/'} component={Home} />
+          <Route path={'brews'} component={Brews} />
         </Router>
       </div>
       <header className='App-header'></header>
